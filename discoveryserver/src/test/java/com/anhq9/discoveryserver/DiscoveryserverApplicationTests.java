7ad63efb03a18e3b1377580ec055105290ffed4f
Nelson An <hoangquangan63@gmail.com>
2025-01-02 22:35:52 +0700

    First Commit

---
+++ discoveryserver/src/test/java/com/anhq9/discoveryserver/DiscoveryserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anhq9.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
